cdd22b126b71353e7fa0e48d1317b23fda76510f
emanuelklado <emanuelklado@msn.com>
2022-03-04 23:09:29 -0300

    requisito 01 ok

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+    render() { 
+        return (
+            <div data-testid="page-login">
+                <p> Login </p>
+            </div>
+        );
+    }
+}
+ 
+export default Login;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render() { 
+        return (
+            <div data-testid="page-not-found">
+                <p>Not Found </p>
+            </div>
+            
+        );
+    }
+}
+ 
+export default NotFound;
