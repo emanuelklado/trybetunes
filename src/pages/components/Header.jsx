2b3ffe15ebbbe454f1a566c312311f4fad756fb5
emanuelklado <emanuelklado@msn.com>
2022-03-05 17:52:44 -0300

    requisito 04 ok

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../../services/userAPI';
 import Loading from '../Loading';
 
@@ -30,7 +31,11 @@ class Header extends Component {
       <>
         <Loading propLoading={ loading } />
         <header className="container-header" data-testid="header-component">
-          <h1>Cabeçalho</h1>
+          <nav>
+            <Link data-testid="link-to-search" to="/search">Busca</Link>
+            <Link data-testid="link-to-favorites" to="/favorites">Musicas Favoritas</Link>
+            <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
+          </nav>
           <p data-testid="header-user-name">
             {' '}
             {`Bem vindo ${userName}`}
